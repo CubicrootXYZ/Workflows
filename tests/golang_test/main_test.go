6c874cdb37fb2ffeba14539ae77974a5124ccd94
cubicroot <github@cubicroot.xyz>
2024-08-11 14:29:53 +0200

    validate env var is set and start db service

--- tests/golang_test/main_test.go
+++ tests/golang_test/main_test.go
@@ -2,9 +2,18 @@ package golangtest_test
 
 import (
 	"golangtest"
+	"os"
 	"testing"
 )
 
 func TestSomething(t *testing.T) {
 	golangtest.DoSomething("a test")
+}
+
+func TestEnvVarsAreSet(t *testing.T) {
+	v := os.Getenv("TESTENV")
+	t.Logf("env var is: %s", v)
+	if v != "value" {
+		t.Fail()
+	}
 }
