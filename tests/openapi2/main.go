aeb1f3b8c5b36a621d1ee3ab620a312d385205d5
cubicroot <github@cubicroot.xyz>
2024-10-06 09:06:50 +0200

    build openapi spec from golang annotations

---
+++ tests/openapi2/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+)
+
+// @title Matrix Reminder and Calendar Bot (RemindMe)
+// @version 2.0.0
+// @description API documentation for the matrix reminder and calendar bot.
+
+// @contact.name Support
+
+// @host your-bot-domain.tld
+// @BasePath /
+// @query.collection.format multi
+
+// @securityDefinitions.apikey APIKeyAuthentication
+// @in header
+// @name Authorization
+func main() {
+	os.Exit(0)
+}
